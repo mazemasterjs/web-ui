6a2fc1648dc0caed235985a1de67bff56f47be3e
James Dean <kreebog@gmail.com>
2019-05-08 18:56:20 -0400

    added _.env

--- src/components/MazeList.vue
+++ src/components/MazeList.vue
@@ -22,58 +22,58 @@
 </template>
 
 <script>
-import axios from 'axios';
-import Logger, { LOG_LEVELS } from '@mazemasterjs/logger';
+  import axios from 'axios';
+  import Logger, { LOG_LEVELS } from '@mazemasterjs/logger';
 
-// configure logger
-const log = Logger.getInstance();
+  // configure logger
+  const log = Logger.getInstance();
 
-export default {
-    name: 'MazeList',
-    data() {
-        return {
-            mazes: [],
-            error: '',
-            title: 'Maze List',
-        };
-    },
-    methods: {},
-    mounted() {
-        const apiUrl = process.env.VUE_APP_API_MAZE_URL + '/get/all';
-        log.debug(__filename, 'getMazes()', 'Mounted.');
-        axios
-            .get(apiUrl)
-            .then((res) => {
-                log.debug(__filename, 'getMazes()', res.data.length + ' Mazes returned.');
-                this.mazes = res.data;
-            })
-            .catch((err) => {
-                log.error(__filename, 'getMazeListData().catch()', 'Error ->', err);
-                this.error = err.message + ' from ' + apiUrl;
-            });
-    },
-};
+  export default {
+      name: 'MazeList',
+      data() {
+          return {
+              mazes: [],
+              error: '',
+              title: 'Maze List',
+          };
+      },
+      methods: {},
+      mounted() {
+          const apiUrl = process.env.VUE_APP_API_MAZE_URL + '/get/all';
+          log.debug(__filename, 'getMazes()', 'Mounted.');
+          axios
+              .get(apiUrl)
+              .then((res) => {
+                  log.debug(__filename, 'getMazes()', res.data.length + ' Mazes returned.');
+                  this.mazes = res.data;
+              })
+              .catch((err) => {
+                  log.error(__filename, 'getMazeListData().catch()', 'Error ->', err);
+                  this.error = err.message + ' from ' + apiUrl;
+              });
+      },
+  };
 </script>
 
 <style>
-.content {
-    margin: auto;
-    border: solid 1px darkslategray;
-}
+  .content {
+      margin: auto;
+      border: solid 1px darkslategray;
+  }
 
-.testing {
-    border: solid 1px green;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    overflow: hidden;
-}
+  .testing {
+      border: solid 1px green;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      overflow: hidden;
+  }
 
-.mazeThumbnail {
-    border: solid 1px red;
-    flex-shrink: 0;
-    height: 100%;
-    width: 100%;
-    /*    font-size: calc(0vw + 0vh + 1vmin); */
-}
+  .mazeThumbnail {
+      border: solid 1px red;
+      flex-shrink: 0;
+      height: 100%;
+      width: 100%;
+      /*    font-size: calc(0vw + 0vh + 1vmin); */
+  }
 </style>
