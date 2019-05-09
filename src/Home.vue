d72c7b7d0a21905c7570c50ff9bddd42fb6e659c
chris <acomputerdog@gmail.com>
2019-05-08 21:50:33 -0400

    Start work on Service API viewer

--- src/Home.vue
+++ src/Home.vue
@@ -4,6 +4,7 @@
       <Header/>
       <v-content>
         <MazeList/>
+        <!--<ServiceViewer/>-->
       </v-content>
     </v-container>
   </v-app>
@@ -12,11 +13,14 @@
 <script>
 import Header from './components/Header';
 import MazeList from './components/MazeList';
+import ServiceViewer from './components/ServiceViewer';
+
 export default {
     name: 'Home',
     components: {
         Header,
         MazeList,
+        ServiceViewer,
     },
 };
 </script>
