5a60f518cdde39a56875297968b35a8b0b4b35a4
Chris Koehler <acomputerdog@gmail.com>
2019-05-18 08:32:32 -0400

    Add side-bar menu and layout page, implement navigation between pages.

---
+++ src/page/Service.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container dark>
+    <ServiceViewer/>
+  </v-container>
+</template>
+
+<script>
+import ServiceViewer from '../components/ServiceViewer.vue';
+
+export default {
+    name: 'ServicePage',
+    components: {
+        ServiceViewer,
+    },
+};
+</script>
+
+<style>
+</style>
