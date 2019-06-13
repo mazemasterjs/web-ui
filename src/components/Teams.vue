f7d3b55b5e8d7a1f4e3c8fd5de87b62c72faf9be
James Dean <kreebog@gmail.com>
2019-06-12 23:35:59 -0400

    replaced external duckduckgo link with material icon

--- src/components/Teams.vue
+++ src/components/Teams.vue
@@ -1,106 +1,107 @@
 <template dark>
-    <div>
-    <br/>
+  <div>
+    <br>
     <h1>TEAMS</h1>
-        <input type="text" v-model="teamName" placeholder="name"/>
-        <input type="text" v-model="teamLogo" placeholder="logo"/>
-        <br/>
-        <input class="button" type="button" @click="createTeam()" value="submit">
-        <template v-for="team in teams">
-
-            <v-list-tile :key="team.id + '-tile'" im>
-            <div id=logo>
-                <img v-bind:src="team.logo"/>
-            </div>
-                <v-list-tile-title v-html="team.name"></v-list-tile-title>
-                BOTS:
-                 <template v-for="bot in team.bots">
-                     <v-list-tile :key="bot.id + '-tile'" im>
-                         <v-list-tile-title v-html="bot.name"></v-list-tile-title>
-                         <v-list-tile-title v-html="bot.coder"></v-list-tile-title>
-                     </v-list-tile>
-                 </template>
-
-                <button v-on:click="deleteTeam(team.id)"><img src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn3.iconfinder.com%2Fdata%2Ficons%2Ftools-solid-icons-vol-2%2F72%2F59-512.png&f=1" style="height: 50px;" alt="Delete team"></button>
-
-            </v-list-tile>
-
+    <input type="text" v-model="teamName" placeholder="name">
+    <input type="text" v-model="teamLogo" placeholder="logo">
+    <br>
+    <input class="button" type="button" @click="createTeam()" value="submit">
+    <template v-for="team in teams">
+      <v-list-tile :key="team.id + '-tile'" im>
+        <div id="logo">
+          <img v-bind:src="team.logo">
+        </div>
+        <v-list-tile-title v-html="team.name"></v-list-tile-title>BOTS:
+        <template v-for="bot in team.bots">
+          <v-list-tile :key="bot.id + '-tile'" im>
+            <v-list-tile-title v-html="bot.name"></v-list-tile-title>
+            <v-list-tile-title v-html="bot.coder"></v-list-tile-title>
+          </v-list-tile>
         </template>
-    <br/>
 
-    </div>
+        <!-- @Siri :: External resource links are unreliable!  Use material.io icons whenever possible: See Chris's post on Trello for samples: https://trello.com/c/YHw9weYI
+        When not possible, download resources and store in the /assets folder (or static-content/images repo) and link to them there.-->
+        <!-- <button v-on:click="deleteTeam(team.id)"><img src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn3.iconfinder.com%2Fdata%2Ficons%2Ftools-solid-icons-vol-2%2F72%2F59-512.png&f=1" style="height: 50px;" alt="Delete team"></button> -->
+        <button icon v-on:click="deleteTeam(team.id)">
+          <v-icon>delete</v-icon>
+        </button>
+      </v-list-tile>
+    </template>
+    <br>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue';
-import axios from 'axios';
-import UUID from 'vue-uuid';
-import { uuid } from 'vue-uuid' // Import uuid
-Vue.use(UUID);
+  import Vue from 'vue';
+  import axios from 'axios';
+  import UUID from 'vue-uuid';
+  import { uuid } from 'vue-uuid'; // Import uuid
+  Vue.use(UUID);
 
-export default {
-     data() {
-            return {
-                teamName: null,
-                teamLogo: null,
-                teams: null,
-                error: null,
-                id: null,
-            };
-        },
-       mounted() {
-        this.getTeams()
-         },
-        methods: {
-            deleteTeam(teamId) {
-            axios.delete('http://mazemasterjs.com/api/team/delete/' + teamId)
-            .then(res => (this.getTeams()))
-            .catch((err) => (console.log(err)))
-        },
-        getTeams() {
-            axios.get('http://mazemasterjs.com/api/team/get')
-            .then(res => (this.teams = res.data))
-            .catch((err) => (console.log(err)))
-        },
-        async createTeam() {
-                await axios.put('http://mazemasterjs.com/api/team/insert', {
-                    id: uuid.v1(),
-                    name: this.teamName,
-                    logo: this.teamLogo,
-                    bots: [],
-                    trophies: []
-                })
-                .then((res) => {
-                    console.log(res)
-                    this.getTeams()
-                })
-                .catch((err)=>{
-                    console.log(err)
-                })
-            }
-        }
-}
+  export default {
+      data() {
+          return {
+              teamName: null,
+              teamLogo: null,
+              teams: null,
+              error: null,
+              id: null,
+          };
+      },
+      mounted() {
+          this.getTeams();
+      },
+      methods: {
+          deleteTeam(teamId) {
+              axios
+                  .delete('http://mazemasterjs.com/api/team/delete/' + teamId)
+                  .then((res) => this.getTeams())
+                  .catch((err) => console.log(err));
+          },
+          getTeams() {
+              axios
+                  .get('http://mazemasterjs.com/api/team/get')
+                  .then((res) => (this.teams = res.data))
+                  .catch((err) => console.log(err));
+          },
+          async createTeam() {
+              await axios
+                  .put('http://mazemasterjs.com/api/team/insert', {
+                      id: uuid.v1(),
+                      name: this.teamName,
+                      logo: this.teamLogo,
+                      bots: [],
+                      trophies: [],
+                  })
+                  .then((res) => {
+                      console.log(res);
+                      this.getTeams();
+                  })
+                  .catch((err) => {
+                      console.log(err);
+                  });
+          },
+      },
+  };
 </script>
 
 <style>
+  .v-list__tile {
+      height: 250px;
+      max-width: 500px;
+  }
 
-.v-list__tile {
-    height: 250px;
-    max-width: 500px;
-}
+  input {
+      border: 1.5px solid white;
+      margin: 5px;
+  }
 
-input{
-    border: 1.5px solid white;
-    margin: 5px;
-}
-
-.button {
-    border: 2px solid black;
-    color: #171717;
-    font-weight: 800;
-    background-color: rgb(65, 181, 161);
-    margin-top: 20px;
-    padding: 10px;
-}
-
+  .button {
+      border: 2px solid black;
+      color: #171717;
+      font-weight: 800;
+      background-color: rgb(65, 181, 161);
+      margin-top: 20px;
+      padding: 10px;
+  }
 </style>
